0596a2cf2a1c69d71feef03c66f78835fdb4daca
noel-lopez <nlopez.publi@gmail.com>
2023-08-19 22:24:45 +0200

    fix: about profile urls

--- pages/about.vue
+++ pages/about.vue
@@ -3,22 +3,19 @@ const devs = [
   {
     url: 'https://owln.ai/',
     name: 'owlnai',
-    imageUrl:
-            'https://avatars.githubusercontent.com/u/20427094?s=64&v=4',
+    imageUrl: 'https://avatars.githubusercontent.com/u/20427094?s=64&v=4',
   },
   {
-    url: 'https://arumxchan.vercel.app/',
+    url: 'https://nlpz-portfolio.vercel.app/',
     name: 'noel-lopez',
-    imageUrl:
-            'https://avatars.githubusercontent.com/u/82571330?s=64&v=4',
+    imageUrl: 'https://avatars.githubusercontent.com/u/82571330?s=64&v=4',
   },
 ]
 const designers = [
   {
-    url: 'https://arumxchan.vercel.app/',
+    url: 'https://www.instagram.com/arumxchan/',
     name: 'arumxchan',
-    imageUrl:
-            'https://i1.sndcdn.com/artworks-bIQHFduN5IM9tz2B-sfAUCw-t500x500.jpg',
+    imageUrl: '/contributors/arumxchan.jpg',
   },
 ]
 </script>
@@ -39,13 +36,13 @@ const designers = [
           <UnoIcon class="i-mingcute-terminal-box-line" /> Desarrollo y código
         </h2>
         <ul class="flex gap-6 mt-6">
-          <li v-for="person in devs" :key="person.name">
-            <NuxtLink :href="`https://github.com/${person.name}`">
-              <div class="flex items-center gap-x-3 bg-white border-2 border-black rounded-xl overflow-hidden pr-3 hover:scale-[.98]">
-                <NuxtImg class-name="w-16 h-16" width="64" height="64" :src="person.imageUrl" alt="" />
+          <li v-for="dev in devs" :key="dev.name">
+            <NuxtLink :href="dev.url" target="_blank" rel="noopener noreferrer">
+              <div class="flex items-center gap-x-3 bg-white border-2 border-black rounded-xl overflow-hidden pr-3 hover:scale-[1.02]">
+                <NuxtImg class-name="w-16 h-16" width="64" height="64" :src="dev.imageUrl" alt="" />
                 <div>
                   <h3 class="text-3xl font-semibold leading-7 font-headings text-yellow-950 comic-text relevance">
-                    @{{ person.name }}
+                    @{{ dev.name }}
                   </h3>
                 </div>
               </div>
@@ -58,13 +55,13 @@ const designers = [
           <UnoIcon class="i-mingcute-paint-brush-line?mask bg-gradient-to-r from-yellow via-yellow-600 to-yellow" /> Ilustración
         </h2>
         <ul class="flex gap-12 mt-6">
-          <li v-for="person in designers" :key="person.name">
-            <NuxtLink :href="`https://github.com/${person.name}`">
-              <div class="flex items-center gap-x-4 bg-white border-2 border-black rounded-xl overflow-hidden pr-3 hover:scale-[.98]">
-                <NuxtImg class-name="w-16 h-16" width="64" height="64" :src="person.imageUrl" alt="" />
+          <li v-for="designer in designers" :key="designer.name">
+            <NuxtLink :href="designer.url" target="_blank" rel="noopener noreferrer">
+              <div class="flex items-center gap-x-4 bg-white border-2 border-black rounded-xl overflow-hidden pr-3 hover:scale-[1.02]">
+                <NuxtImg class-name="w-16 h-16" width="64" height="64" :src="designer.imageUrl" alt="" />
                 <div>
                   <h3 class="text-3xl font-semibold leading-7 font-headings text-yellow-950 comic-text relevance">
-                    @{{ person.name }}
+                    @{{ designer.name }}
                   </h3>
                 </div>
               </div>
