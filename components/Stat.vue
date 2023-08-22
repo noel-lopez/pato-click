6fb17471c7054f3e04a2293f0e0181eea45fc9c2
noel-lopez <nlopez.publi@gmail.com>
2023-08-22 16:52:49 +0200

    feat: add achievements in stats page

--- components/Stat.vue
+++ components/Stat.vue
@@ -1,16 +1,16 @@
 <script setup lang="ts">
-const props = defineProps<{ title: string; content: number }>()
-const { currencyToString } = useFormat()
-const formatedCurrency = computed(() => currencyToString(props.content))
+const props = defineProps<{ title: string; content: string | number }>()
 </script>
 
 <template>
-  <article class="flex flex-col-reverse bg-white p-4 border-black border-2 rounded-xl">
+  <article class="flex flex-col-reverse bg-white p-4 border-black border-2 rounded-xl min-w-[300px]">
     <header>
-      <h2>{{ title }}</h2>
+      <h2 class="text-lg">
+        {{ title }}
+      </h2>
     </header>
     <main class="text-3xl font-bold text-emerald-900">
-      {{ formatedCurrency }}
+      {{ content }}
     </main>
   </article>
 </template>
--- pages/stats.vue
+++ pages/stats.vue
@@ -1,7 +1,25 @@
-<script setup>
+<script setup lang="ts">
+import type { AchievementType } from '../index.d.ts'
 import { useStore } from '~/store/main'
+import { useFormat } from '~/composables/useFormat'
 
 const state = useStore()
+const { currencyToString } = useFormat()
+const formattedEarnedCash = computed(() => currencyToString(state.earnedCash))
+const formattedMoneySpent = computed(() => currencyToString(state.moneySpent))
+
+function getAchievementCategoryTitle(type: AchievementType) {
+  switch (type) {
+    case 'cash':
+      return 'Logros de patomonedas'
+    case 'itemLevel':
+      return 'Logros de items'
+    case 'easteregg':
+      return 'Easter eggs'
+    case 'rare':
+      return 'Logros raros'
+  }
+}
 </script>
 
 <template>
@@ -9,14 +27,24 @@ const state = useStore()
     <h1 class="flex items-center my-8 text-5xl font-bold font-headings text-emerald-950">
       <pato-coin />{{ state.cash }}
     </h1>
-    <header class="flex flex-col items-center justify-between gap-2 w-2xl">
-      <h2 class="text-3xl font-bold font-headings text-emerald-9/90">
-        Consulta tus estadísticas
-      </h2>
-      <div class="flex gap-4 my-4">
-        <Stat title="Dinero ganado" :content="state.earnedCash" />
-        <Stat title="Dinero gastado" :content="state.moneySpent" />
+    <header class="flex flex-col justify-between gap-2 w-2xl">
+      <!-- <h2 class="text-3xl font-bold font-headings text-emerald-9/90">
+        Recuento de datos
+      </h2> -->
+      <div class="my-4 grid grid-cols-1 gap-4 md:grid-cols-2">
+        <Stat title="Patomonedas ganadas" :content="formattedEarnedCash" />
+        <Stat title="Patomonedas gastadas" :content="formattedMoneySpent" />
+        <Stat title="Logros desbloqueados" :content="state.earnedAchievementsCount" />
+        <Stat title="Niveles de items" :content="state.itemLevelsCount" />
       </div>
     </header>
+    <section v-for="achievementCategory in state.achievementsList" :key="achievementCategory.type" class="my-4">
+      <h3 class="text-3xl font-bold font-headings text-emerald-9/90 py-3">
+        {{ getAchievementCategoryTitle(achievementCategory.type) }}
+      </h3>
+      <div class="grid grid-cols-1 gap-4 lg:grid-cols-2">
+        <Achievement v-for="achievement in achievementCategory.achievements" :key="achievement.id" v-bind="achievement" />
+      </div>
+    </section>
   </div>
 </template>
