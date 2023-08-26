96618184bf748b1176531515f050243db461a224
noel-lopez <nlopez.publi@gmail.com>
2023-08-27 01:01:31 +0200

    fix: stats card text selection

--- components/Stat.vue
+++ components/Stat.vue
@@ -3,14 +3,14 @@ const props = defineProps<{ title: string; content: string | number }>()
 </script>
 
 <template>
-  <article class="flex flex-col-reverse bg-white p-4 border-neutral-600 border-2 rounded-xl min-w-[300px] dark:bg-teal-300/10 dark:border-teal-200">
-    <header>
-      <h2 class="text-lg">
-        {{ title }}
-      </h2>
-    </header>
+  <article class="flex flex-col bg-white p-4 border-neutral-600 border-2 rounded-xl min-w-[300px] dark:bg-teal-300/10 dark:border-teal-200">
     <main class="text-3xl font-bold text-emerald-900 dark:text-emerald-300">
       {{ content }}
     </main>
+    <footer>
+      <h2 class="text-lg">
+        {{ title }}
+      </h2>
+    </footer>
   </article>
 </template>
